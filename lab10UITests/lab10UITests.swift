219768fd9da0ee1ba592f9bc6f423432f4996870
ICraZZyBoY <73648312+ICraZZyBoY@users.noreply.github.com>
2024-05-31 18:11:03 +0300

    Update lab10UITests.swift

--- lab10UITests/lab10UITests.swift
+++ lab10UITests/lab10UITests.swift
@@ -21,6 +21,23 @@ final class lab10UITests: XCTestCase {
 
         let enterButton = app.buttons["Вход"]
         XCTAssert(enterButton.exists)
-        
+        enterButton.tap()
+        let segmentedControl = app.segmentedControls["signInOrUp"]
+        XCTAssert(segmentedControl.exists)
+        let regButton = segmentedControl.buttons.element(boundBy: 0)
+        XCTAssert(regButton.exists)
+        regButton.tap()
+        let loginInput = app.textFields["loginInput"]
+        XCTAssert(loginInput.exists)
+        loginInput.tap()
+        loginInput.typeText("1")
+        let passwordInput = app.secureTextFields["passwordInput"]
+        XCTAssert(passwordInput.exists)
+        passwordInput.tap()
+        passwordInput.typeText("1")
+        let emailInput = app.textFields["emailInput"]
+        XCTAssert(emailInput.exists)
+        emailInput.tap()
+        emailInput.typeText("1")
     }
 }
